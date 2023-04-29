904f885d2e435de5894331ee5288660da174b7e6
spaceinfuser <wsyu@ryerson.ca>
2023-04-29 17:28:30 -0400

    adding scale
    adding scale scene, added grouping to player and box asset

---
+++ scenes/scale/scale.gd
@@ -0,0 +1,23 @@
+extends StaticBody3D
+
+var counter = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("boxes"):
+		counter += 1
+	print (counter)
+
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("boxes"):
+		counter -= 1
+	print (counter) 
