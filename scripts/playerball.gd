05cc19d9994be9fe20053870b0dc3731fc19da54
Alari Tork <alari.tork@gmail.com>
2023-04-29 14:25:20 -0400

    Improved Player Controller

--- scripts/playerball.gd
+++ scripts/playerball.gd
@@ -1,8 +1,12 @@
 extends RigidBody3D
 
-var push_force = 50.0
-var force: Vector3
-var jump_force = 600.0
+
+@export var push_force = 50.0
+@export var jump_force = 600.0
+
+
+var _force_dir: Vector3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +15,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	apply_force(force * push_force, Vector3.ZERO)
+	apply_force(_force_dir * push_force, Vector3.ZERO)
 	
 func _input(event):
-	var input_dir = Input.get_vector("ui_up", "ui_down", "ui_left", "ui_right")
-	force = Vector3(input_dir.y, 0, input_dir.x)
-	if event.is_action_pressed("ui_accept"):
+	var input_dir = Input.get_vector("move_up", "move_down", "move_left", "move_right")
+	_force_dir = Vector3(input_dir.y, 0, input_dir.x)
+	if event.is_action_pressed("jump"):
 		apply_force(jump_force * Vector3.UP, Vector3.ZERO)
