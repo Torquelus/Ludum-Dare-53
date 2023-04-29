9e52f4842660c5dd2f5f547ac0c9c9075912ac9a
Taniya <tpeterratnaraj@gmail.com>
2023-04-29 16:50:01 -0400

    Finished Conveyor Prototype

--- scenes/conveyor/conveyor.gd
+++ scenes/conveyor/conveyor.gd
@@ -1,27 +1,18 @@
 extends StaticBody3D
 
 
-signal applyConveyorForce
-signal disableConveyorForce
+signal applied_conveyor_force
+signal disabled_conveyor_force
 
-@export var conveyorDirection: Vector3 = Vector3(0,0,1);
-@export var conveyorStrength: int = 6;
+@export var conveyor_direction: Vector3 = Vector3(0,0,1);
+@export var conveyor_strength: int = 2;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	constant_linear_velocity = conveyor_direction*conveyor_strength
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
 
-func _on_conveyor_area_body_entered(body):
-	print("good")
-	applyConveyorForce.emit(conveyorDirection*conveyorStrength)
-
-
-
-func _on_conveyor_area_body_exited(body):
-	disableConveyorForce.emit()
