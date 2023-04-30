49cadd5f5765ce704079cb7b1baa3c0adb359d71
vfewa <vfewa@users.noreply.github.com>
2023-04-30 13:01:39 -0400

    Added sounds to player movement
    Janky player sound with movement over certain velocity

---
+++ Audio/PlayerSounds.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#func _input(event):
+#	print_debug("._222")
+#	if(playing!= true && event!=null):
+#			play()
+#	elif(playing==true && PlayerController.:
+#		volume_db = volume_db-10
+#		stop()
+#		volume_db = volume_db+10
