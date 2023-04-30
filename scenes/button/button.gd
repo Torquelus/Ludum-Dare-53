ceb29b8fc146091f948b6609411b707494bcfdb2
t2peterr <t2peterr@cs.ryrson.ca>
2023-04-30 09:21:58 -0400

    conveyor and levers and buttons all linkable

--- scenes/button/button.gd
+++ scenes/button/button.gd
@@ -1,12 +1,17 @@
 extends Node3D
 
 @onready var button_anim = $AnimationPlayer
-var pressed: bool = false
+
+@export var target:Node
+signal button_pushed
+
+var pushed: bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if target:
+		button_pushed.connect(target.enable_process)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,16 +19,14 @@ func _process(delta):
 
 
 func _on_area_3d_body_entered(body):
-	pressed = true
+	pushed = true
 	button_anim.play("button_pushed")
 
 func _on_area_3d_body_exited(body):
-	pressed = false
+	pushed = false
 	button_anim.play_backwards("button_pushed")
 
 
 func _on_animation_player_animation_finished(anim_name):
-	if pressed == true:
-		print("button pressed")
-	else:
-		print("noPress")
+	if pushed == true:
+		button_pushed.emit()
--- scenes/conveyor/conveyor.gd
+++ scenes/conveyor/conveyor.gd
@@ -1,17 +1,28 @@
 extends StaticBody3D
 
+@export_category("Conveyer's Lever Setup Variables ")
+@export var reverse: bool = false
+@export var enable: bool = false
+@export var disable: bool = false
 
-signal applied_conveyor_force
-signal disabled_conveyor_force
-
-@export var conveyor_direction: Vector3 = Vector3(0,0,1);
-@export var conveyor_strength: int = 2;
+@export_category("Conveyer Speed and Direction")
+@export var conveyer_direction: Vector3 = Vector3(0,0,0)
+@export var conveyor_strength: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	constant_linear_velocity = conveyor_direction*conveyor_strength
-	pass
+	constant_linear_velocity = conveyer_direction*conveyor_strength
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func enable_process():
+	if reverse == true:
+		constant_linear_velocity = constant_linear_velocity*-1
+	elif enable == true:
+		constant_linear_velocity = conveyer_direction*conveyor_strength
+	elif disable == true:
+		constant_linear_velocity = Vector3.ZERO
+
