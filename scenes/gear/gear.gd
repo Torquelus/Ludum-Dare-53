cdfefd96eaecee9111640d6cb89aead2d117fb68
Nada <nada.eissa@gmail.com>
2023-04-30 19:01:30 -0400

    gears are working!! custom duration, direction, and axis included in dev UI

--- scenes/gear/gear.gd
+++ scenes/gear/gear.gd
@@ -1,12 +1,14 @@
 extends StaticBody3D
 
 @export var rotation_duration = 5.0
+@export var rotation_direction = 360.0
+@export_enum("x", "y", "z") var rotation_axis = "x"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var tween = create_tween()
 	tween.set_loops()
-	tween.tween_property(self, "rotation_degrees:x", 360.0, rotation_duration).as_relative()
+	tween.tween_property(self, "rotation_degrees:" + rotation_axis, rotation_direction, rotation_duration).as_relative()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
