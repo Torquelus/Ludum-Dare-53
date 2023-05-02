8edc615f050772f41883374eac40d284c70d2f2b
wsyu <wsyu@cs.ryerson.ca>
2023-05-01 20:19:37 -0400

    adding my levels

--- scenes/scale/scale.gd
+++ scenes/scale/scale.gd
@@ -1,9 +1,14 @@
 extends StaticBody3D
+@export var target:Node3D
 
 var counter = 0
 signal correct
+signal incorrect
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if target:
+		correct.connect(target.enable_process)
+		incorrect.connect(target.disable_process)
 	pass # Replace with function body.
 
 
@@ -26,8 +31,9 @@ func _on_area_3d_body_exited(body):
 func update_scale():
 	$Display.mesh.set_text(str("%d Boxes" % counter))
 	var mat = $Display.get_active_material(0)
-	if counter == 4:
+	if counter == 1:
 		correct.emit()
 		mat.albedo_color = Color8(84,143,109,255)
 	else:
+		incorrect.emit()
 		mat.albedo_color = Color8(0,0,0,255)
