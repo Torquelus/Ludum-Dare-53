523da8b5f3c2caa4af3cbb03e66ad0067c906164
vfewa <vfewa@users.noreply.github.com>
2023-05-01 22:15:40 -0400

    Update volume.gd
    added lv_4

--- scripts/menu_and_gui/volume.gd
+++ scripts/menu_and_gui/volume.gd
@@ -3,4 +3,4 @@ class_name GlbVolumeClass
 extends Node
 
 var GlobalVolume = 50
-var GlobalLevelSelect = ["res://scenes/levels/storage_levels/level1.tscn","res://scenes/levels/storage_levels/level2.tscn" ,"res://scenes/levels/storage_levels/level4.tscn","res://scenes/levels/storage_levels/level5.tscn","res://scenes/levels/storage_levels/EndScreen.tscn"]
+var GlobalLevelSelect = ["res://scenes/levels/storage_levels/level1.tscn","res://scenes/levels/storage_levels/level2.tscn" ,"res://scenes/levels/storage_levels/level4.tscn","res://scenes/levels/storage_levels/level_4.tscn", "res://scenes/levels/storage_levels/level5.tscn","res://scenes/levels/storage_levels/EndScreen.tscn"]
